454d8850e1fde552be426e3a8c4b49b1c513b5fb
Dzima Pooh <102469519+kodzimo@users.noreply.github.com>
2022-12-08 16:19:05 +0300

    kind up

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -1,23 +1,21 @@
 #! /bin/bash
 
 # General preparations
-sudo yum makecache && yum upgrade
-sudo yum install -y vim net-tools
+yum makecache && yum upgrade -y
+yum install -y vim net-tools
 
 # Docker installation
 # curl -fsSL get.docker.com -o get-docker.sh
 # sudo sh get-docker.sh
 
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo systemctl start docker
-sudo docker run hello-world
-
+yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+systemctl start docker
+sleep 3
 
 # Docker configuration
-sudo groupadd docker
 sudo usermod -aG docker vagrant
 newgrp docker
 
@@ -27,7 +25,7 @@ sudo systemctl enable containerd.service
 # Kind installation
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
 chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
+mv ./kind /usr/bin/kind
 
 # Kind cluster creation
 kind create cluster --config /vagrant/k8s/kind-ingress-config.yaml
